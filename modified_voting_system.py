983a97a6ed6283807733211edbd56685c90508dd
LianKarylGarcia <liankarylg@gmail.com>
2025-01-28 21:22:12 +0800

    I used the .upper() method to capitalize the lines I wanted to be in uppercase.

--- modified_voting_system.py
+++ modified_voting_system.py
@@ -8,7 +8,8 @@ nominee2 = input("Enter the name of the 2nd nominee: ").strip()  # Remove leadin
 nm1_votes = 0
 nm2_votes = 0
 
-voter_id = list(range(1, 11))  # Simplified voter ID generation
+voter_id = [1,2,3,4,5,6,7,8,9,10]
+
 no_of_voter = len(voter_id)
 
 while True:
@@ -16,11 +17,11 @@ while True:
         print("Voting session is over !!!")
         if nm1_votes> nm2_votes:
             percent = (nm1_votes/no_of_voter)*100 #to calculate the percentage
-            print(nominee1,"has won the election with",percent,"% of votes")
+            print(f"{nominee1.title()} has won the election with {percent:.2f}% of votes!".upper())  # Capitalized winner name
             break # to get rid of infinite loop
         elif nm2_votes> nm1_votes:
             percent = (nm2_votes/no_of_voter)*100 #to calculate the percentage
-            print(nominee2,"has won the election with",percent,"% of votes")
+            print(f"{nominee2.title()} has won the election with {percent:.2f}% of votes!".upper())  # Capitalized winner name
             break # to get rid of infinite loop
         else:
             print("Both have equal number of votes !!! Government will decide who will rule")
@@ -29,7 +30,7 @@ while True:
 
     voter = int(input("Enter your voter id : "))
     if voter in voter_id:
-        print("You are a voter")
+        print("You are a voter".upper())
         voter_id.remove(voter) # we will remove so that agin same voter can't vote
         print("-------------------------------------")
         print("To give vote to", nominee1,"Press 1  ")
@@ -38,13 +39,12 @@ while True:
         vote = int(input("Enter you precious vote : "))
         if vote == 1:
             nm1_votes +=1
-            print(nominee1,"Thank you to give your important vote to them !!")
-        elif vote == 2:
+            print("Thank you to give your important vote to them !!".upper()) # To print it in uppercase         elif vote == 2:
             nm2_votes +=1
-            print(nominee2,"Thank you to give your important vote to them !!")
+            print("Thank you to give your important vote to them !!".upper()) # To print it in uppercase
         elif vote > 2:
-            print("Check your pressed key !!")
+            print("Check your pressed key !!".upper()) # To print it in uppercase
         else:
-            print("You are not a voter OR You have already voted")
+            print("You are not a voter OR You have already voted".upper()) # To print it in uppercase
 
 
